0f8791300a77d3ad04d19965bbd14034e132b288
Adriano Monteiro <adrianomonteiro@pop-os.localdomain>
2022-05-10 21:37:47 -0300

    NEW: selector condition for whatsapp destop or mobile.

--- src/services/linkGenerateService.js
+++ src/services/linkGenerateService.js
@@ -2,13 +2,13 @@ const { linkGenerateHandler, errorConstructor } = require("../utils/functions");
 const { checkLinkGenerate } = require("../utils/schemaValidation");
 
 const linkGenerateService = async (requestBody) => {
-  const { whatsapp, message } = requestBody;
+  const { whatsapp, message, type } = requestBody;
 
   const { error } = checkLinkGenerate.validate({ whatsapp, message });
 
   if (error) return errorConstructor(401, error.message);
 
-  const linkWhatsapp = await linkGenerateHandler(whatsapp, message);
+  const linkWhatsapp = await linkGenerateHandler(whatsapp, message, type);
 
   return linkWhatsapp;
 };
