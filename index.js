ac28ec85ba280b93d0635c7e6daa71fdb11ec765
Adriano Monteiro <adrianomonteiroweb@mail.com>
2022-05-11 17:00:47 -0300

    FIX: env settings.

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 const express = require("express");
-// const cors = require("cors");
+const cors = require("cors");
+require("dotenv").config();
 
 const routes = require("./src/routes");
 
 const app = express();
 
 app.use(express.json());
-// app.use(cors());
+app.use(cors());
 
 app.use(routes);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3338;
 
 app.listen(PORT, () => {
   if (process.env.PORT) {
